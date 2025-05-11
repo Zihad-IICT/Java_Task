672bcd64fd74b0be84e95fcd2ad31572ef919ffd
Zihad-IICT <zihad171162@gmail.com>
2025-05-11 16:09:27 +0600

    Create ReverseString.java

---
+++ ReverseString.java
@@ -0,0 +1,11 @@
+package PrcaticePrbOOP;
+
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        StringBuffer sb=new StringBuffer("Zihad");
+        sb.reverse();
+        System.out.println(sb);
+    }
+}
